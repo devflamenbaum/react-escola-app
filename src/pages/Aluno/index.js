fd59f6bb2c27589fbbf69e257dd1c765903e713d
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-08-20 09:34:27 -0300

    Cadastrando Aluno #1

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -1,13 +1,79 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
-function Aluno() {
+function Aluno({ match }) {
+    const id = get(match, 'params.id', 0);
+    const [aluno, setAluno] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <Container>
-            <h1>Aluno</h1>
+            <h1>{id ? 'Editar Aluno' : 'Novo Aluno'}</h1>
+
+            <Form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={aluno}
+                    onChange={(e) =>
+                        setAluno({ ...Aluno, nome: e.target.value })
+                    }
+                    placeholder="Nome"
+                />
+                <input
+                    type="email"
+                    value={aluno}
+                    onChange={(e) =>
+                        setAluno({ ...Aluno, email: e.target.value })
+                    }
+                    placeholder="Email"
+                />
+                <input
+                    type="number"
+                    value={aluno}
+                    onChange={(e) =>
+                        setAluno({ ...Aluno, idade: e.target.value })
+                    }
+                    placeholder="Idade"
+                />
+                <input
+                    type="text"
+                    value={aluno}
+                    onChange={(e) =>
+                        setAluno({ ...Aluno, peso: e.target.value })
+                    }
+                    placeholder="Peso"
+                />
+                <input
+                    type="text"
+                    value={aluno}
+                    onChange={(e) =>
+                        setAluno({ ...Aluno, altura: e.target.value })
+                    }
+                    placeholder="Altura"
+                />
+                <input
+                    type="text"
+                    value={aluno}
+                    onChange={(e) =>
+                        setAluno({ ...Aluno, nome: e.target.value })
+                    }
+                    placeholder="Nome"
+                />
+
+                <button type="submit">Enviar</button>
+            </Form>
         </Container>
     );
 }
+
+Aluno.propTypes = {
+    match: PropTypes.shape({}).isRequired,
+};
 
 export default Aluno;
